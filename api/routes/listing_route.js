dfeee1b7ca4a08071bb25dfb4f4bad9d60ffed47
Aayush Kumar <aayushkumar9376@gmail.com>
2024-07-10 22:51:05 +0530

    add the complete update listing functionality

--- api/routes/listing_route.js
+++ api/routes/listing_route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../utility/verifyUser.js';
-import { createListing, deleteListing, updateListing } from '../controllers/listing_controller.js';
+import { createListing, deleteListing, updateListing, getListing } from '../controllers/listing_controller.js';
 
 const router = express.Router();
 
 router.post('/create', createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
+router.get('/get/:id', getListing);
 
 export default router;
--- user/src/App.jsx
+++ user/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 import Header from "./components/Header.jsx";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Profile from "./pages/Profile.jsx";
 import SignIn from "./pages/Sign_In.jsx";
 import SignUp from "./pages/Sign_Up.jsx";
-import PrivateRoute from "./components/PrivateRoute.jsx";
 import CreateListing from "./pages/CreateListing.jsx";
+import UpdateListing from "./pages/UpdateListing.jsx";
 
 export default function App() {
   return (
@@ -17,9 +18,10 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route element={<PrivateRoute/>}>
+        <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
-          <Route path="/create-listing" element={<CreateListing />}/>
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/update-listing/:listingId" element={<UpdateListing />} />
         </Route>
       </Routes>
     </Router>
