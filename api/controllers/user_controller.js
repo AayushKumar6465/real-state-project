c03ed82ac82b3c3496d1d2084e032e927324baa7
Aayush Kumar <aayushkumar9376@gmail.com>
2024-07-10 09:10:40 +0530

    create get user listing api route

--- api/controllers/user_controller.js
+++ api/controllers/user_controller.js
@@ -1,5 +1,6 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user_model.js";
+import Listing from "../models/listing_model.js";
 import { errorHandler } from "../utility/error_utility.js";
 
 export const test = (req, res) => {
@@ -7,13 +8,13 @@ export const test = (req, res) => {
 }
 
 export const updateUser = async (req, res, next) => {
-    if (req.user.id !== req.params.id) 
+    if (req.user.id !== req.params.id)
         return next(errorHandler(401, "You can only update your own account!"));
     try {
         if (req.body.password) {
-        req.body.password = bcryptjs.hashSync(req.body.password, 10);
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
         }
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, 
+        const updatedUser = await User.findByIdAndUpdate(req.params.id,
             {
                 $set: {
                     username: req.body.username,
@@ -24,17 +25,17 @@ export const updateUser = async (req, res, next) => {
             }, { new: true });
 
         const { password, ...rest } = updatedUser._doc;
-        
+
         res.status(200).json(rest);
-    
+
     } catch (error) {
         console.error(error);
         next(error);
     }
 };
 
-export const deleteUser = async(req, res, next) => {
-    if (req.user.id !== req.params.id) 
+export const deleteUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id)
         return next(errorHandler(401, "You can only delete your own account!"));
     try {
         await User.findByIdAndDelete(req.params.id);
@@ -43,5 +44,18 @@ export const deleteUser = async(req, res, next) => {
     } catch (error) {
         console.log(error);
         next(error);
+    }
+};
+
+export const getUserListings = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+        try {
+            const listings = await Listing.find({ userRef: req.params.id });
+            res.status(200).json(listings);
+        } catch (error) {
+            return next(error);
+        }
+    } else {
+        return next(errorHandler(401, 'You can only view your own listings!'));
     }
 };
--- api/routes/user_route.js
+++ api/routes/user_route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { updateUser, test, deleteUser } from '../controllers/user_controller.js';
+import { updateUser, test, deleteUser, getUserListings } from '../controllers/user_controller.js';
 import { verifyToken } from '../utility/verifyUser.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/test', test);  
 router.post('/update-user/:id', verifyToken, updateUser);
 router.delete('/delete-user/:id', verifyToken, deleteUser);
+router.get('/listings/:id', verifyToken, getUserListings);
 
 export default router;
