a07e51da1908e9e743bbc6a35b9ed8b3c70c0754
Aayush Kumar <aayushkumar9376@gmail.com>
2024-07-11 23:42:07 +0530

    Creating the UI of search page

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -7,6 +7,7 @@ import Profile from "./pages/Profile.jsx";
 import SignIn from "./pages/Sign_In.jsx";
 import SignUp from "./pages/Sign_Up.jsx";
 import Listing from "./pages/Listing.jsx";
+import Search from "./pages/Search.jsx";
 import CreateListing from "./pages/CreateListing.jsx";
 import UpdateListing from "./pages/UpdateListing.jsx";
 
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/listing/:listingId" element={<Listing />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
